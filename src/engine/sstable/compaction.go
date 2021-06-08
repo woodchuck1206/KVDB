7a5202cde9d28b97e7b7bacac76b238bba399950
woodchuckchoi <choiggoggi@gmail.com>
2021-06-08 22:55:37 +0900

    multi merge in progress

--- src/engine/sstable/compaction.go
+++ src/engine/sstable/compaction.go
@@ -1,9 +1,9 @@
 package sstable
 
 import (
-	"io"
 	"os"
 
+	"github.com/woodchuckchoi/KVDB/src/engine/util"
 	"github.com/woodchuckchoi/KVDB/src/engine/vars"
 )
 
@@ -16,54 +16,117 @@ type MergeSignal struct {
 	LevelRef *SSTable
 }
 
-type MergeUnit struct {
-	Buffer []byte
-	Index  int
-	Init   bool
+type MergeUnit struct { // per Block
+	KeyValue []vars.KeyValue
+
+	SparseIndex []vars.SparseIndex
+	SparseIdx   int // sparse index's index
+
 	Finish bool
 	File   *os.File
 }
 
-func (this *MergeUnit) Load() { // load size should be managed // each key value pairs sizes differ
-	_, err := this.File.Read(this.Buffer)
-	this.Index = 0
-	if err == io.EOF {
+func (this *MergeUnit) Load() {
+	// load size should be managed // each key value pairs sizes differ
+	var curBufferSize int
+
+	switch {
+	case this.SparseIdx+1 == len(this.SparseIndex):
+		fileInfo, _ := this.File.Stat()
+		curBufferSize = int(fileInfo.Size()) - this.SparseIndex[this.SparseIdx].Offset
+
+	case this.SparseIdx >= len(this.SparseIndex):
 		this.Finish = true
+		return
+
+	default: // this.SparseIdx < len(this.SparseIndex) - 1
+		curBufferSize = this.SparseIndex[this.SparseIdx+1].Offset - this.SparseIndex[this.SparseIdx].Offset
 	}
+
+	this.SparseIdx++
+
+	buffer := make([]byte, curBufferSize)
+
+	this.File.Read(buffer)
+	this.KeyValue = util.ByteSliceToKeyValue(buffer)
 }
 
-func (this *MergeUnit) Get() (byte, error) { // should return keyvalue pair
-	var ret byte
+func (this *MergeUnit) Get() (vars.KeyValue, error) {
+	var ret vars.KeyValue
+
+	if len(this.KeyValue) == 0 {
+		this.Load()
+	}
 	if this.Finish {
 		return ret, vars.FILE_EOF_ERROR
 	}
 
-	if this.Index >= len(this.Buffer) || !this.Init {
-		this.Init = true
-		this.Index = 0
-		this.Load()
-	}
-
-	ret = this.Buffer[this.Index]
-	// if ret...
+	ret = this.KeyValue[0]
+	return ret, nil
 }
 
-func (this *MergeUnit) Pop() (byte, error) {
+func (this *MergeUnit) Pop() (vars.KeyValue, error) {
 	ret, err := this.Get()
-	this.Index++
+	if err == nil {
+		this.KeyValue = this.KeyValue[1:]
+	}
 	return ret, err
 }
 
 func NewCompactor(chanBuffer int) (*Compactor, chan<- MergeSignal) {
 	if chanBuffer <= 0 {
-		chanBuffer = 42 // arbitrary default number for buffer size
+		chanBuffer = 42 // arbitrary default number for MergeSignal buffer size
 	}
 	channel := make(chan MergeSignal, chanBuffer)
 	return &Compactor{Receiver: channel}, channel
 }
 
-func MultiMerge(level *Level) Block {
+func MultiMerge(level *Level, l int) Block {
+	mergeUnits := []MergeUnit{}
+	mergedKeyValues := []vars.KeyValue{}
 
+	for _, block := range level.Blocks {
+		file, err := os.Open(block.FileName)
+		if err == nil {
+			mergeUnits = append(mergeUnits, MergeUnit{
+				KeyValue:    []vars.KeyValue{},
+				SparseIndex: block.Index,
+				SparseIdx:   0,
+				Finish:      false,
+				File:        file,
+			})
+		}
+	}
+
+	// multi-merge
+	for {
+		var unitWithSmallestKeyValue *MergeUnit
+		nextMergeUnits := []MergeUnit{}
+		for i := 0; i < len(mergeUnits); i++ {
+			keyValue, err := mergeUnits[i].Get()
+			if err != nil { // EOF
+				continue
+			}
+			nextMergeUnits = append(nextMergeUnits, mergeUnits[i])
+			if unitWithSmallestKeyValue != nil {
+				curSmallestKeyValue, _ := unitWithSmallestKeyValue.Get()
+				if curSmallestKeyValue.Key > keyValue.Key {
+					unitWithSmallestKeyValue = &(mergeUnits[i])
+				}
+			}
+		}
+
+		if unitWithSmallestKeyValue == nil {
+			break
+		}
+		toAdd, _ := unitWithSmallestKeyValue.Pop()
+		mergedKeyValues = append(mergedKeyValues, toAdd)
+		mergeUnits = nextMergeUnits
+	}
+
+	// the mergedKeyValues should not keep all records obviously, it should write as it reads
+	// util.KeyValueSliceToByteSliceAndSparseIndex()
+	// util.WriteKeyValuePairs()
 }
 
 // func (this *Compactor) Run() {
