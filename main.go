5ed80b4a176d7fc538eca72c36e945742f2275ad
woodchuckchoi <choiggoggi@gmail.com>
2021-07-02 00:16:33 +0900

    server & client more to go
    - Forgot the BloomFilter!

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	_ "github.com/woodchuckchoi/KVDB/src/engine"
+	"github.com/woodchuckchoi/KVDB/src/controller"
+	"github.com/woodchuckchoi/KVDB/src/engine"
 )
 
 func main() {
+	engine := engine.NewEngine(2048, 3)
+	server := controller.Init(engine)
+	server.Run()
 }
--- src/controller/controller.go
+++ src/controller/controller.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/woodchuckchoi/KVDB/src/engine"
 	"github.com/woodchuckchoi/KVDB/src/engine/vars"
@@ -11,36 +13,65 @@ type KVServer struct {
 	Echo   *echo.Echo
 }
 
-func Init(engine *engine.Engine) KVServer {
-	e := echo.New()
-	mapEcho(e)
+type Response struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+	Error string `json:"error"`
+}
 
-	return KVServer{
-		Engine: engine,
-		Echo:   e,
+func generateResponse(key, value string, err error) *Response {
+	if err != nil {
+		return &Response{
+			Error: err.Error(),
+		}
+	}
+	return &Response{
+		Key:   key,
+		Value: value,
 	}
 }
 
-func (this KVServer) Run() {
+func Init(engine *engine.Engine) *KVServer {
+	e := echo.New()
+	kvServer := &KVServer{
+		Engine: engine,
+		Echo:   e,
+	}
+	kvServer.mapEcho()
+	return kvServer
+}
+
+func (this *KVServer) Run() {
 	this.Echo.Logger.Fatal(this.Echo.Start(vars.DEFAULT_PORT))
 }
 
-func mapEcho(e *echo.Echo) {
-	e.GET("/get/:key", get)
-	e.PUT("/put", put)
-	e.DELETE("/del", del)
+func (this *KVServer) mapEcho() {
+	this.Echo.GET("/get/:key", this.get)
+	this.Echo.POST("/put", this.put)
+	this.Echo.DELETE("/del/:key", this.del)
 }
 
-func put(c echo.Context) error {
+func (this *KVServer) put(c echo.Context) error {
 	key := c.FormValue("key")
 	value := c.FormValue("value")
 
+	err := this.Engine.Put(key, value)
+	resp := generateResponse(key, value, err)
+	return c.JSON(http.StatusOK, resp)
 }
 
-func get(c echo.Context) error {
+func (this *KVServer) get(c echo.Context) error {
 	key := c.Param("key")
+
+	value, err := this.Engine.Get(key)
+	resp := generateResponse(key, value, err)
+	return c.JSON(http.StatusOK, resp)
 }
 
-func del(c echo.Context) error {
+func (this *KVServer) del(c echo.Context) error {
+	key := c.Param("key")
 
+	err := this.Engine.Delete(key)
+	resp := generateResponse(key, "", err)
+	return c.JSON(http.StatusOK, resp)
 }
