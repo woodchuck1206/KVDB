8ca57b8446dbecd2256698aab128a06a9b4c7232
woodchuckchoi <choiggoggi@gmail.com>
2021-07-05 23:46:51 +0900

    client in progress, which is not the most impressive thing...

--- src/client/client.go
+++ src/client/client.go
@@ -2,8 +2,16 @@ package client
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
+)
+
+const (
+	BASE_URL string = "localhost:7777"
+	GET_URL  string = BASE_URL + "/get/"
+	PUT_URL  string = BASE_URL + "/put"
+	DEL_URL  string = BASE_URL + "/del/"
 )
 
 type Command struct {
@@ -20,20 +28,57 @@ const (
 	DEL
 )
 
+type Response struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+	Error string `json:"error"`
+}
+
 func intro() {
 	fmt.Println("Hello GOKVDB!")
 }
 
 func parseArgs(ss []string) {
-	switch strings.ToLower(ss[0]) {
+	cmd := ss[0]
+	ss = ss[1:]
+	switch strings.ToLower(cmd) {
 	case "get":
-
+		get(ss)
 	case "put":
 
 	case "del":
 
 	}
 
+}
+
+func get(args []string) {
+	if len(args) != 1 {
+		fmt.Println("USAGE ERROR!")
+		return
+	}
+	resp, err := http.Get(GET_URL + args[0])
+	if err != nil {
+		fmt.Println("NETWORK ERROR!")
+	}
+	fmt.Println(resp)
+}
+
+func put(args []string) {
+	if len(args) != 2 {
+		fmt.Println("USAGE ERROR!")
+		return
+	}
+	resp, err := http.Post(PUT_URL, "application/json")
+}
+
+func del(args []string) {
+	if len(args) != 1 {
+		fmt.Println("USAGE ERROR!")
+		return
+	}
+
+	resp, err := http.NewRequest(http.MethodDelete, DEL_URL+args[0])
 }
 
 func main() {
